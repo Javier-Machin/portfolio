cd1a937c8c226aeaa24e3fdfa3072ea16108779f
Herman Brown <javier_machin_acosta@hotmail.com>
2018-10-21 13:57:07 +0100

    Updates

---
+++ precache-manifest.7f7d7e8213d6fe92186c7392d59cb9e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "db6c8c1d3c3f525c67dc",
+    "url": "/portfolio/static/js/main.db6c8c1d.chunk.js"
+  },
+  {
+    "revision": "26155ee75c3305ca0ef7",
+    "url": "/portfolio/static/js/1.26155ee7.chunk.js"
+  },
+  {
+    "revision": "db6c8c1d3c3f525c67dc",
+    "url": "/portfolio/static/css/main.d5a94881.chunk.css"
+  },
+  {
+    "revision": "c480ea9213f3add3dd893363f766a278",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.dbf04c1a4fa44ebfd174d86889bf6de9.js"
+  "/portfolio/precache-manifest.7f7d7e8213d6fe92186c7392d59cb9e0.js"
 );
 
 workbox.clientsClaim();
