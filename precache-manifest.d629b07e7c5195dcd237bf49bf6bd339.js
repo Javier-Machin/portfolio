673a5ff4c117c31c117d9f6f17f528cbf2367d1d
Javier Machin <javier_machin_acosta@hotmail.com>
2018-10-25 13:59:55 +0100

    Updates

---
+++ precache-manifest.d629b07e7c5195dcd237bf49bf6bd339.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "799c81329f8e146d109b96134fd194c7",
+    "url": "/portfolio/static/media/profile.799c8132.jpg"
+  },
+  {
+    "revision": "387b5093df48b1bf430dbc5237303a53",
+    "url": "/portfolio/static/media/Invader2s.387b5093.jpg"
+  },
+  {
+    "revision": "8a83bad6a05940ed61b3ed87c72f93e8",
+    "url": "/portfolio/static/media/Invader1s.8a83bad6.jpg"
+  },
+  {
+    "revision": "ee6a878fdf535e4fe3aeb1ede33a6478",
+    "url": "/portfolio/static/media/Invader.ee6a878f.gif"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "a95147937451fb3f5ba0",
+    "url": "/portfolio/static/js/main.a9514793.chunk.js"
+  },
+  {
+    "revision": "0a23e1c1d257e53bec94",
+    "url": "/portfolio/static/js/1.0a23e1c1.chunk.js"
+  },
+  {
+    "revision": "a95147937451fb3f5ba0",
+    "url": "/portfolio/static/css/main.3291b8f3.chunk.css"
+  },
+  {
+    "revision": "a1212b6befb94a5fff2d7bdb88342d00",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.9a902537cdc8397aa8423613fe187d46.js"
+  "/portfolio/precache-manifest.d629b07e7c5195dcd237bf49bf6bd339.js"
 );
 
 workbox.clientsClaim();
