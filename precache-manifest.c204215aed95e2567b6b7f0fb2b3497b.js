a08753f205c1ce89824045bc67bdeb079655e7e5
Javier M <javier_machin_acosta@hotmail.com>
2020-09-28 22:19:41 +0100

    Updates

---
+++ precache-manifest.c204215aed95e2567b6b7f0fb2b3497b.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cd89caedaf02060e624fbeeeb05f06f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d07fc29e29e1bf46a75",
+    "url": "/static/css/main.ad7883d1.chunk.css"
+  },
+  {
+    "revision": "810ef6a8e56539abaf3e",
+    "url": "/static/js/2.78d38bca.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.78d38bca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d07fc29e29e1bf46a75",
+    "url": "/static/js/main.108e8564.chunk.js"
+  },
+  {
+    "revision": "0934e5a0629121a12638",
+    "url": "/static/js/runtime-main.c542aba9.js"
+  },
+  {
+    "revision": "88d2eac652de68428ccbe94805745f34",
+    "url": "/static/media/Beatroot-1.88d2eac6.jpg"
+  },
+  {
+    "revision": "a1d488c3cd0da9dbcf52ca447156a3e3",
+    "url": "/static/media/Beatroot-2.a1d488c3.jpg"
+  },
+  {
+    "revision": "f55d520158b3173d6331ca30adcbcf47",
+    "url": "/static/media/Beatroot-3.f55d5201.jpg"
+  },
+  {
+    "revision": "7078b5b104defce82f292c0eb076aa80",
+    "url": "/static/media/Flightbooker1s.7078b5b1.jpg"
+  },
+  {
+    "revision": "47a05365d3ab432d0d828d3b1fd0a678",
+    "url": "/static/media/Flightbooker2s.47a05365.jpg"
+  },
+  {
+    "revision": "236381a2d6692865b15606099bbbcf70",
+    "url": "/static/media/Flightbooker3s.236381a2.jpg"
+  },
+  {
+    "revision": "388bdbf3144913665b2f1a099b071885",
+    "url": "/static/media/Insendi-1.388bdbf3.jpg"
+  },
+  {
+    "revision": "588c8dd905fdacc3bc60f8c4beb5b1c6",
+    "url": "/static/media/Insendi-2.588c8dd9.jpg"
+  },
+  {
+    "revision": "21b3b179660b96cc0cb8d5183f40b4e7",
+    "url": "/static/media/Insendi-3.21b3b179.jpg"
+  },
+  {
+    "revision": "e206fbf85842d9d58717bbfe1decb99a",
+    "url": "/static/media/Invader.e206fbf8.gif"
+  },
+  {
+    "revision": "171aced7b1942c6a373e0b0a2b509d6e",
+    "url": "/static/media/Invader1s.171aced7.jpg"
+  },
+  {
+    "revision": "ca8724b33f95c1506a06ee7bbdc8a993",
+    "url": "/static/media/Invader2s.ca8724b3.jpg"
+  },
+  {
+    "revision": "d949d8a1f9fa748b2bc1507040f25ffc",
+    "url": "/static/media/Lanzaroteoverview1s.d949d8a1.jpg"
+  },
+  {
+    "revision": "641dd2c3429565873a64b8f5bbd6dc96",
+    "url": "/static/media/Lanzaroteoverview2s.641dd2c3.jpg"
+  },
+  {
+    "revision": "187b82a4b29b196e41a2f5cdc225b9fd",
+    "url": "/static/media/Reactreadlist1s.187b82a4.jpg"
+  },
+  {
+    "revision": "2e27309185cc9d58850bdbf539b11502",
+    "url": "/static/media/Reactreadlist2s.2e273091.jpg"
+  },
+  {
+    "revision": "d3f468ad07cf9c65025655e0948ac9ad",
+    "url": "/static/media/Reactreadlist3.d3f468ad.gif"
+  },
+  {
+    "revision": "4e1f9203c6b426f37c25b5bb8b5abd7a",
+    "url": "/static/media/Reactweather1s.4e1f9203.jpg"
+  },
+  {
+    "revision": "dd1d10685da76d18191b88f21c221a56",
+    "url": "/static/media/Reactweather2s.dd1d1068.jpg"
+  },
+  {
+    "revision": "34be4c16dd374944fb2ead0e16186cd9",
+    "url": "/static/media/Reactweather3.34be4c16.gif"
+  },
+  {
+    "revision": "2265c532c5c567e5ae270803e0d640cc",
+    "url": "/static/media/Socialboost1s.2265c532.jpg"
+  },
+  {
+    "revision": "fbb8f12b900581c596e0a358ac57753b",
+    "url": "/static/media/Socialboost2s.fbb8f12b.jpg"
+  },
+  {
+    "revision": "41cdb4e3bbb012d4291c404f6c75bf09",
+    "url": "/static/media/Socialboost3s.41cdb4e3.jpg"
+  },
+  {
+    "revision": "250f3721387ac396c6c1dbd84ed48c5e",
+    "url": "/static/media/TOP.250f3721.png"
+  },
+  {
+    "revision": "3f6e68877f66cc9987fcbb203a7cac15",
+    "url": "/static/media/Visualbasicnet.3f6e6887.png"
+  },
+  {
+    "revision": "fa5a02dcbc1994457c6915773598772a",
+    "url": "/static/media/graphql.fa5a02dc.png"
+  },
+  {
+    "revision": "e5e23835409fede38bf57ef7ed3ce20c",
+    "url": "/static/media/profile.e5e23835.jpg"
+  }
+]);
